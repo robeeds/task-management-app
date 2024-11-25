91c08560ef0a858bd454d8226cbc5fad7938110a
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-11-24 18:27:44 -0800

    Added Login Page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,55 @@
+'use client'
+
+import { motion } from "motion/react"
+import Image from "next/image"
+
+import login from "../../../public/login.svg"
+import { log } from "console"
+
 
 export default function Login() {
   return (  
-  <div>This is the login page</div>
+  <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
+
+    <div className="flex flex-col gap-6 row-start-2 items-center sm:items-start bg-backgroundTwo p-10 rounded-[20px] md:min-w-[600px]">
+
+      {/* Form Title */}
+      <p className="self-center font-semibold text-[32px]">Login</p>
+      <hr className="flex flex-1 w-full border-foreground rounded-full" />
+
+
+      {/* This will be the Username Field */}
+      <div className="flex flex-1 flex-col w-full gap-1">
+        <p className="font-medium">Username</p>
+        <input type="text" id="username" autoComplete="true" className="bg-background p-2 rounded-[10px]"/>
+      </div>
+      
+
+      {/* This will be the Password Field */}
+      <div className="flex flex-1 flex-col w-full gap-1">
+        <p className="font-medium">Password</p>
+        <input type="password" id="password" autoComplete="true" className="bg-background p-2 rounded-[10px]"/>
+      </div>
+
+      {/* This will be the login button */}
+      <motion.button
+        className="self-center bg-lime rounded-full my-6"
+        whileHover={{ scale: 1.1 }}
+      >
+        <Image
+          src={login}
+          width="32"
+          height="32"
+          alt="Login Button"
+          className="m-2.5"
+        />
+      </motion.button>
+
+      <p className="self-center text-foregroundTwo">
+        Don't have an account? Sign up <a href="/signup" className="text-blue underline">here</a>
+      </p>
+
+    </div>
+  </div>
   )
 }
