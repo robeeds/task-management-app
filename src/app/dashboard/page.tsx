7194ef5a61c969e3ca914e7ee5422669a3917c89
robeeds <71895118+robeeds@users.noreply.github.com>
2024-11-30 21:30:48 -0800

    Add dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+//mport { logOutUser } from "@/lib/server/appwrite"
+
+
+export default function Dashboard() {
+    return(
+        <div>
+           <p>Dashboard </p>
+            <button //onClick={logOutUser}
+            >Logout</button>
+        </div>
+    )
+}
