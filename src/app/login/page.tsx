ffbcadcb4b4a709d3d343d52b70697b52821e1d0
robeeds <71895118+robeeds@users.noreply.github.com>
2024-11-30 21:30:16 -0800

    Add basic auth, TO BE FIXED

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,19 @@
-'use client'
-
-import { motion } from "motion/react"
 import Image from "next/image"
-
 import login from "../../../public/login.svg"
-import { log } from "console"
 
+import { getLoggedInUser, logInUser } from "@/lib/server/appwrite"
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+  const user = await getLoggedInUser();
+  if (user) {
+    redirect("/dashboard");
+  }
+
   return (  
   <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
 
-    <div className="flex flex-col gap-6 row-start-2 items-center sm:items-start bg-backgroundTwo p-10 rounded-[20px] md:min-w-[600px]">
+    <form action={logInUser} className="flex flex-col gap-6 row-start-2 items-center sm:items-start bg-backgroundTwo p-10 rounded-[20px] md:min-w-[600px]">
 
       {/* Form Title */}
       <p className="self-center font-semibold text-[32px]">Login</p>
@@ -20,21 +22,22 @@ export default function Login() {
 
       {/* This will be the Username Field */}
       <div className="flex flex-1 flex-col w-full gap-1">
-        <p className="font-medium">Username</p>
-        <input type="text" id="username" autoComplete="true" className="bg-background p-2 rounded-[10px]"/>
+        <p className="font-medium">Email</p>
+        <input type="email" id="email" name="email" autoComplete="true" className="bg-background p-2 rounded-[10px]"/>
       </div>
       
 
       {/* This will be the Password Field */}
       <div className="flex flex-1 flex-col w-full gap-1">
         <p className="font-medium">Password</p>
-        <input type="password" id="password" autoComplete="true" className="bg-background p-2 rounded-[10px]"/>
+        <input type="password" id="password" name="password" autoComplete="true" className="bg-background p-2 rounded-[10px]"/>
       </div>
 
       {/* This will be the login button */}
-      <motion.button
+      <button
+        type="submit"
         className="self-center bg-lime rounded-full my-6"
-        whileHover={{ scale: 1.1 }}
+        //whileHover={{ scale: 1.1 }}
       >
         <Image
           src={login}
@@ -43,13 +46,13 @@ export default function Login() {
           alt="Login Button"
           className="m-2.5"
         />
-      </motion.button>
+      </button>
 
       <p className="self-center text-foregroundTwo">
         Don't have an account? Sign up <a href="/signup" className="text-blue underline">here</a>
       </p>
 
-    </div>
+    </form>
   </div>
   )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,45 +1,49 @@
-'use client'
-
-import { motion } from "motion/react"
 import Image from "next/image"
-
 import login from "../../../public/login.svg"
 
+import { signUpWithEmail } from "@/lib/server/appwrite"
 
-export default function SignUp() {
+export default async function SignUp() {
+
   return (  
   <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
 
-    <div className="flex flex-col gap-6 row-start-2 items-center sm:items-start bg-backgroundTwo p-10 rounded-[20px] md:min-w-[600px]">
+    <form action={signUpWithEmail} className="flex flex-col gap-6 row-start-2 items-center sm:items-start bg-backgroundTwo p-10 rounded-[20px] md:min-w-[600px]">
 
       {/* Form Title */}
       <p className="self-center font-semibold text-[32px]">Sign Up</p>
       <hr className="flex flex-1 w-full border-foreground rounded-full" />
 
-
+      
       {/* This will be the Username Field */}
       <div className="flex flex-1 flex-col w-full gap-1">
         <p className="font-medium">Username</p>
-        <input type="text" id="username" autoComplete="true" className="bg-background p-2 rounded-[10px]"/>
+        <input id="username" name="username" type="username" className="bg-background p-2 rounded-[10px]"/>
+      </div>
+
+      {/* This will be the Email Field */}
+      <div className="flex flex-1 flex-col w-full gap-1">
+        <p className="font-medium">Email</p>
+        <input id="email" name="email" type="email" autoComplete="true" className="bg-background p-2 rounded-[10px]"/>
       </div>
       
 
       {/* This will be the Password Field */}
       <div className="flex flex-1 flex-col w-full gap-1">
         <p className="font-medium">Password</p>
-        <input type="password" id="password" autoComplete="true" required className="bg-background p-2 rounded-[10px]"/>
+        <input id="password" name="password" minLength={8} type="password" autoComplete="true" required className="bg-background p-2 rounded-[10px]"/>
       </div>
 
       {/* This will be the Confirm Password Field */}
       <div className="flex flex-1 flex-col w-full gap-1">
         <p className="font-medium">Confirm Password</p>
-        <input type="password" id="password" autoComplete="true" className="bg-background p-2 rounded-[10px]"/>
+        <input id="confirmPass" name="confirmPass" type="password" className="bg-background p-2 rounded-[10px]"/>
       </div>
 
       {/* This will be the login button */}
-      <motion.button
+      <button
+        type="submit"
         className="self-center bg-lime rounded-full my-6"
-        whileHover={{ scale: 1.1 }}
       >
         <Image
           src={login}
@@ -48,13 +52,13 @@ export default function SignUp() {
           alt="Login Button"
           className="m-2.5"
         />
-      </motion.button>
+      </button>
 
       <p className="self-center text-foregroundTwo">
         Have an account? Log in <a href="/login" className="text-blue underline">here</a>
       </p>
 
-    </div>
+    </form>
   </div>
   )
 }
