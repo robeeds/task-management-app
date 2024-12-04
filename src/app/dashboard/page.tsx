d360caec7f2d7344a14b8101169e797cc108dfda
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-12-04 10:43:57 -0800

    Create basic dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,27 @@
-//mport { logOutUser } from "@/lib/server/appwrite"
+import { createSessionClient, getLoggedInUser } from "@/lib/server/appwrite";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
+async function logout() {
+    "use server";
 
-export default function Dashboard() {
+    const { account}  = await createSessionClient();
+
+    const nextCookies = await cookies();
+    nextCookies.delete("my-custom-session");
+    await account.deleteSession("current");
+
+    redirect("/");
+}
+
+export default async function Dashboard() {
+    const user = await getLoggedInUser();
+    if (!user) redirect('/login');
+
     return(
         <div>
-           <p>Dashboard </p>
-            <button //onClick={logOutUser}
+           <p>Dashboard: Logged in as {user.name}</p>
+            <button onClick={logout}
             >Logout</button>
         </div>
     )
