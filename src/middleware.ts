9327688ff30563b2743bddb874af2fdbd7f4f8c2
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-12-05 16:29:53 -0800

    Update middleware redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,15 +8,11 @@ export async function middleware(request: NextRequest) {
     let cookie = await nextCookies.get("user-session"); // Adjust so that the session is set to the Appwrite user-session cookie
 
     // If the user is already logged in (i.e., session exists), redirect to the dashboard
-    if (cookie) {
-        return NextResponse.redirect(new URL('/dashboard', request.url));
+    if (cookie && (request.nextUrl.pathname.startsWith('/login') || request.nextUrl.pathname.startsWith('/signup'))) {
+      return NextResponse.redirect(new URL('/dashboard', request.url))
     }
 
-    // Continue to the login page if no session is found
-    return NextResponse.next();
+    if (!cookie && request.nextUrl.pathname.startsWith('/dashboard')) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
 }
-
-// Specify the matching routes for this middleware
-export const config = {
-  matcher: '/login', // Apply middleware only on the /login page
-};
